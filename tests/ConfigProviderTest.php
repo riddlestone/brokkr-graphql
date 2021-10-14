fc97feb8583037f59723555447e69f7cd40e8fa0
Andrew Riddlestone <andrew.riddlestone@gmail.com>
2021-10-14 12:37:33 +0100

    chore(tests): Added ConfigProvider tests

---
+++ tests/ConfigProviderTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Riddlestone\Brokkr\GraphQL\Test;
+
+use Riddlestone\Brokkr\GraphQL\ConfigProvider;
+use PHPUnit\Framework\TestCase;
+
+class ConfigProviderTest extends TestCase
+{
+    /**
+     * @covers \Riddlestone\Brokkr\GraphQL\ConfigProvider::__invoke
+     */
+    public function test__invoke()
+    {
+        $configProvider = new ConfigProvider();
+        $this->assertEquals(
+            ['dependencies', 'graphql_fields', 'graphql_types', 'router'],
+            array_keys($configProvider())
+        );
+    }
+
+    /**
+     * @covers \Riddlestone\Brokkr\GraphQL\ConfigProvider::getDependencyConfig
+     */
+    public function testGetDependencyConfig()
+    {
+        $configProvider = new ConfigProvider();
+        $this->assertEquals(
+            ['aliases', 'factories'],
+            array_keys($configProvider->getDependencyConfig())
+        );
+    }
+
+    /**
+     * @covers \Riddlestone\Brokkr\GraphQL\ConfigProvider::getGraphQLFieldConfig
+     */
+    public function testGetGraphQLFieldConfig()
+    {
+        $configProvider = new ConfigProvider();
+        $this->assertEquals(
+            ['abstract_factories'],
+            array_keys($configProvider->getGraphQLFieldConfig())
+        );
+    }
+
+    /**
+     * @covers \Riddlestone\Brokkr\GraphQL\ConfigProvider::getGraphQLTypeConfig
+     */
+    public function testGetGraphQLTypeConfig()
+    {
+        $configProvider = new ConfigProvider();
+        $this->assertEquals(
+            ['abstract_factories'],
+            array_keys($configProvider->getGraphQLTypeConfig())
+        );
+    }
+
+    /**
+     * @covers \Riddlestone\Brokkr\GraphQL\ConfigProvider::getRouteConfig
+     */
+    public function testGetRouteConfig()
+    {
+        $configProvider = new ConfigProvider();
+        $routeConfig = $configProvider->getRouteConfig();
+        $this->assertEquals(
+            ['routes'],
+            array_keys($routeConfig)
+        );
+        $this->assertEquals(
+            ['graphql'],
+            array_keys($routeConfig['routes'])
+        );
+    }
+}
